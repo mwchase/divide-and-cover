e9b90bcb3c23b1c7c0890833af5e527fe8458228
Max Chase <max.chase@gmail.com>
2018-07-22 20:45:14 -0400

    Bump version

--- runner/setup.py
+++ runner/setup.py
@@ -15,7 +15,7 @@ with c_open(path.join(HERE, 'README.rst'), encoding='utf-8') as f:
 setup(
     name='divide_and_cover',
 
-    version='0.6.0',
+    version='0.6.1',
 
     description='Picky test coverage runner',
     long_description=LONG_DESCRIPTION,
